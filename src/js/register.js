24c01a0befef44c25c99e1e4a58928304256e726
lealr <seepdbr@hotmail.com>
2023-03-21 22:36:36 -0300

    atualização do proj:  - register.js > CONCLUÍDO

--- src/js/register.js
+++ src/js/register.js
@@ -1,3 +1,4 @@
+
 var name;
 var lastName;
 var email;
@@ -16,13 +17,17 @@ function register(){
     email = document.getElementById('email').value;
     cel = document.getElementById('cel').value;
     password = document.getElementById('password').value;
-
     address = document.getElementById('address').value;
     number = document.getElementById('number').value;
     zipCode = document.getElementById('zip-code').value;
     complement = document.getElementById('complement').value;
-
-    alert(toString());
+    sex = document.formUser.sex;
+    if(!validation() && !validationSex()){
+        var print = document.getElementById('text-container');
+        print.innerHTML = "Preencha os campos corretamente";
+}else{
+        alert(toString());
+    }
 }
 
 function toString(){
@@ -33,4 +38,37 @@ function toString(){
     "\nNúmero: " + number +
     "\nCEP: " + zipCode +
     "\nComplemento: " + complement;
+}
+
+function validation(){
+    nameBol = name.length > 2;
+    lastNameBol = lastName.length > 2;
+    emailBol = email.length >= 10;
+    celBol = cel.length == 11;
+    passwordBol = password.length >= 3;
+    addressBol = address.length >= 7;
+    numberBol = number.length > 0;
+    zipCodeBol = zipCode.length == 8;
+
+    if(nameBol && lastNameBol && emailBol && celBol && passwordBol && addressBol && numberBol && zipCodeBol){
+        
+        var print = document.getElementById('text-container');
+        print.innerHTML = "Cadastrador com sucesso";
+        console.log('caiu no sucess')
+    }else{
+        var print = document.getElementById('text-container');
+        print.innerHTML = "Preencha os campos corretamente";
+    }
+    
+}
+
+function validationSex(){
+    var sex = document.formUser.sex
+    var print = document.getElementById('text-container');
+    
+    if(!sex[0].checked && !sex[1].checked && !sex[2].checked )
+        print.innerHTML = "Erro, Selecione um sexo";
+        else{
+            return true;
+        }
 }
